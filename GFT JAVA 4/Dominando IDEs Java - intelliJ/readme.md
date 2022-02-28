f30be82ef8273376064fe79f3471aa160548a1b6
antonioqneto <antoniodevneto@gmail.com>
2022-02-28 15:34:13 -0300

    Primeiro commit pelo IntelliJ

---
+++ GFT JAVA 4/Dominando IDEs Java - intelliJ/readme.md
@@ -0,0 +1,9 @@
+# Alguns Atalhos Intellij
+
+* Refatorar nomes = Shift + F6 (Eclipse = F2)
+* Shift + F12
+* Ctrl + Alt + L = Corrige indentações
+* Ctrl + D = Duplica linha
+* Ctrl + Shift + Seta cima/baixo = desloca local da linha
+* https://resources.jetbrains.com/storage/products/intellij-idea/docs/IntelliJIDEA_ReferenceCard.pdf
+* 
---
+++ GFT JAVA 4/Dominando IDEs Java - intelliJ/src/br/com/dio/PrimeiroProgramaIntellij.java
@@ -0,0 +1,18 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroProgramaIntellij {
+
+/*    public static void main(String[] args) {
+        System.out.println("Olar, mundo");
+    }
+    Para comentar blocos, usar Ctrl + Shift + / (varias linhas) ou Ctrl + / (comenta as linhas)
+    */
+
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        // se comentar o trecho e usar Ctrl + Alt + O a IDE remove as importações desnecessárias
+    }
+
+}
